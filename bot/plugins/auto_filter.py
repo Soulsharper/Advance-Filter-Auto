ff72dc41d4ca4f7bb2d981a93b902efdd65f3de5
Amal Joy <86186525+Soulsharper@users.noreply.github.com>
2021-09-22 16:49:54 +0530

    Update auto_filter.py

--- bot/plugins/auto_filter.py
+++ bot/plugins/auto_filter.py
@@ -60,6 +60,12 @@ async def auto_filter(bot, update):
     filters = await db.get_filters(group_id, query)
     
     if filters:
+        results.append(
+                [
+                    InlineKeyboardButton("📢 Join My Main Channel 📢", url=f"https://t.me/{MT_CHANNEL}")
+                ]
+            )
+    if filters:
         for filter in filters: # iterating through each files
             file_name = filter.get("file_name")
             file_type = filter.get("file_type")
