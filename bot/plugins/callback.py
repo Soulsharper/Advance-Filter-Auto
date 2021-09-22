62d53ba740876336e7ec0ffbf4b31a72bc1b537f
Amal Joy <86186525+Soulsharper@users.noreply.github.com>
2021-09-22 17:06:33 +0530

    Update callback.py

--- bot/plugins/callback.py
+++ bot/plugins/callback.py
@@ -142,7 +142,7 @@ async def cb_navg(bot, update: CallbackQuery):
     
     reply_markup = InlineKeyboardMarkup(temp_results)
     
-    text=f"<i>JOIN @numberonemovieworld 😍</i> <code>{leng}</code> <i>Results For Your Request 📂:</i> <code>{query}</code>"
+    text=f"<b>🗂️Total File :- {leng}</b>\n<b>🎬Movie Name :-</b> <code>{query}</code>"
         
     try:
         await update.message.edit(
